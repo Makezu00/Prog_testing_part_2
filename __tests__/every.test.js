cc87cdd8b8c64712991b90d35dbf7fec540843e4
Markus <markus.hissa@hotmail.com>
2024-11-15 14:56:24 +0200

    First every.js tests

--- __tests__/every.test.js
+++ __tests__/every.test.js
@@ -1,1 +1,8 @@
 const every = require('../src/every');
+
+test('Happy cases', () => {
+    expect(every([], Boolean)).toBeTruthy();
+    expect(every([true, 1, null, 'yes'], Boolean)).toBeFalsy();
+    expect(every([1,1,1], Number)).toBeTruthy();
+    expect(every(['y', 'y'], String)).toBeTruthy();
+});

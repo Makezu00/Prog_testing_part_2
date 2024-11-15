8114bba5c7d99c9b78fbd77f810e34123fdb4c55
Markus <markus.hissa@hotmail.com>
2024-11-15 17:10:09 +0200

    First filter test

--- __tests__/filter.test.js
+++ __tests__/filter.test.js
@@ -1,1 +1,10 @@
 const filter = require('../src/filter');
+
+test('Happy cases', () => {
+    const users = [
+        { 'user': 'barney', 'active': true },
+        { 'user': 'fred',   'active': false }
+        ];
+    expect(filter(users, ({active}) => active))
+    .toStrictEqual([{ 'user': 'barney', 'active': true }]);
+});
